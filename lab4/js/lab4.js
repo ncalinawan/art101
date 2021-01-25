ef6430591f03003d5bae37a78151558be9562eb5
Noel Calinawan <ncalinaw@ucsc.edu>
2021-01-24 21:50:57 -0800

    Lab 4

---
+++ lab4/js/lab4.js
@@ -0,0 +1,13 @@
+// Make Buttons for each choice
+    // Rock Button
+    // Paper Button
+    // Scissors Button
+// Assign buttons values
+    // Rock = 1
+    // Paper = 2
+    // Scissors = 3
+// Accept mouse input for each button
+// Set player value to button they press.
+// Randomly select number 1-3 for opponent
+// Display opponent choice in text
+// Test for win condition (2 > 1, 3 > 2, 1 > 3)
