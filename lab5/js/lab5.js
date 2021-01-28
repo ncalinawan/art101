6f67d127ce63dbe53cdb4bc60ffa09909bd425c7
Noel Calinawan <ncalinaw@ucsc.edu>
2021-01-27 22:03:13 -0800

    Lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+/*
+* Author: Noel Calinawan <ncalinaw@ucsc.edu>
+* Created: 27 January
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Mercedes-Benz";
+model = "SLK 320";
+color = "Black";
+year = 2001;
+var ownIt = true;
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Owned: " + ownIt + "<br>");
