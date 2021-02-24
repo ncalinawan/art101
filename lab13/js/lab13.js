58ece527c3a972abe934950a305ca055e07392ab
Noel Calinawan <ncalinaw@ucsc.edu>
2021-02-24 11:36:45 -0800

    lab 13

---
+++ lab13/js/lab13.js
@@ -0,0 +1,47 @@
+/*
+  Author: Noel Calinawan
+  Created: 02.25.2021
+ */
+var str = "";
+function fizzBuzz() {
+  for (var i = 1; i <= 200; i++) {
+    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+      str = i + " FizzBuzzBoom";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "fizzbuzzboom")
+    } else if (i % 3 == 0 && i % 5 == 0) {
+      str = i + " FizzBuzz";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "fizzbuzz")
+    } else if (i % 3 == 0 && i % 7 == 0) {
+      str = i + " FizzBoom";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "fizzboom")
+    } else if (i % 5 == 0 && i % 7 == 0) {
+      str = i + " BuzzBoom";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "buzzboom")
+    } else if (i % 3 == 0) {
+      str = i + " Fizz";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "fizz");
+    } else if (i % 5 == 0) {
+      str = i + " Buzz";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "buzz");
+    } else if (i % 7 == 0) {
+      str = i + " Boom";
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i + "boom");
+    } else {
+      str = i;
+      $("#output").append("<p>" + str + "</p>");
+      console.log(i)
+    }
+  }
+
+
+}
+
+
+fizzBuzz();
