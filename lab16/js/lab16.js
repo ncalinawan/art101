91aec5d8e1a5c458182a38547e3fc43d43fcdb89
ncalinawan <63563322+ncalinawan@users.noreply.github.com>
2021-03-08 10:09:47 -0800

    lab 16 test

---
+++ lab16/js/lab16.js
@@ -0,0 +1,31 @@
+/*
+  Author: Noel Calinawan
+  Created: 03.09.2021
+ */
+
+ // Using the core $.ajax() method
+ $.ajax({
+     // The URL for the request (from the api docs)
+     url: "http://xkcd.com/info.0.json",
+     // The data to send (will be converted to a query string)
+     data: {
+             // here is where any data required by the api
+             //   goes (check the api docs)
+
+           },
+     // Whether this is a POST or GET request
+     type: "GET",
+     // The type of data we expect back
+     dataType : "json",
+     // What do we do when the api call is successful
+     //   all the action goes in here
+     success: function(data) {
+         $('#output-text').html("<h2>" + data.title);
+         $('#output-image').html("<img src=" + data.img + "title=" + data.alt + ">");
+     },
+     // What we do if the api call fails
+     error: function (jqXHR, textStatus, errorThrown) {
+         // do stuff
+         console.log("Error:", textStatus, errorThrown);
+     }
+ })
